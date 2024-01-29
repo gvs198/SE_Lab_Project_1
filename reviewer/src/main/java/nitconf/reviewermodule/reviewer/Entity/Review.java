45450cbbac034f2cf4d0cf13d03092877f31ce72
Yajat Ponakampalli <yponakampalli@gmail.com>
2024-01-29 20:49:56 +0530

    Entity changed

--- reviewer/src/main/java/nitconf/reviewermodule/reviewer/Entity/Review.java
+++ reviewer/src/main/java/nitconf/reviewermodule/reviewer/Entity/Review.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "reviews")
 public class Review {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
